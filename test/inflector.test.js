49ead5358bc84b3b482e5ae60aa4982a9197d150
Bakyt B <b3bakyt@gmail.com>
2019-10-16 13:32:04 +0600

    test passed - The function pluralize should return right exception plural

--- test/inflector.test.js
+++ test/inflector.test.js
@@ -13,6 +13,20 @@ describe('pluralize tests', () => {
         expect(Inflector.pluralize('issue')).equal('issues');
         expect(Inflector.pluralize('box')).equal('boxes');
         expect(Inflector.pluralize('apple')).equal('apples');
+        expect(Inflector.pluralize('easy')).equal('easies');
+        done();
+    });
+
+    it('The function pluralize should return right exception plural', (done) => {
+        expect(Inflector.pluralize('atlas')).equal('atlases');
+        expect(Inflector.pluralize('brother')).equal('brothers');
+        expect(Inflector.pluralize('cookie')).equal('cookies');
+        expect(Inflector.pluralize('move')).equal('moves');
+        expect(Inflector.pluralize('genie')).equal('genies');
+        expect(Inflector.pluralize('loaf')).equal('loaves');
+        expect(Inflector.pluralize('mythos')).equal('mythoi');
+        expect(Inflector.pluralize('corps')).equal('corps');
+        expect(Inflector.pluralize('flounder')).equal('flounder');
         done();
     });
 });
