aca80241336809f0a9cfdff8369eb86dbbef9398
Bakyt B <b3bakyt@gmail.com>
2019-10-18 18:05:13 +0600

    test failed - The function underscore should convert CamelCase to underscored words

---
+++ test/underscore.test.js
@@ -0,0 +1,17 @@
+const Inflector = require('../index');
+const Chai = require('chai');
+const {
+    expect,
+    assert,
+    should,
+} = Chai;
+
+
+describe('underscore tests', () => {
+    it('The function underscore should convert CamelCase to underscored words', (done) => {
+        expect(Inflector.underscore('TestWord')).equal('test_word');
+        expect(Inflector.underscore('testWord_Hello')).equal('test_word__hello');
+        expect(Inflector.underscore('TestWord-Hello123Hi')).equal('test_word-hello123_hi');
+        done();
+    });
+});
