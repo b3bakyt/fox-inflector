196ea9c4e54ad0f2b701c7e5059d86a25bb793c9
Bakyt B <b3bakyt@gmail.com>
2019-10-19 17:44:32 +0600

    fixed variablize tests

--- test/variablize.test.js
+++ test/variablize.test.js
@@ -11,7 +11,7 @@ describe('variablize tests', () => {
     it('The function variablize should return same as camelize but first char is in lowercase', (done) => {
         expect(Inflector.variablize('test_word_id')).equal('testWordId');
         expect(Inflector.variablize('user_group_states')).equal('userGroupStates');
-        expect(Inflector.variablize('user_group_states123', true)).equal('userGroupStates123');
+        expect(Inflector.variablize('user_group_states123')).equal('userGroupStates123');
         done();
     });
 });
